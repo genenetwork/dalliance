a936025c00a36bb9a2331fa59aaa2002413eeda7
Christian Fischer <christian@chfi.se>
2016-05-24 19:48:08 +0200

    Add CSV test

---
+++ test/csv-test.js
@@ -0,0 +1,31 @@
+/* -*- mode: javascript; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+"use strict";
+
+var loadCsv = require('../js/csv.js').loadCsv;
+
+describe('CSV files', function() {
+    // currently serving this using Caddy; needs CORS
+    var csvURI = 'http://localhost:8000/grav2_geno.csv';
+    var csv;
+
+    it('can be created by connecting to a URI', function(done) {
+        csv = loadCsv(csvURI, done);
+        console.log("Test CSV object is truthy");
+        expect(csv).toBeTruthy();
+    });
+
+    // TODO: make sure the retrieved data actually is correct
+    it('can retrieve lines from an interval', function(done) {
+        csv = loadCsv(csvURI, function() {});
+        csv.fetch(100, 151, function(seq, err) {
+            console.log("Test error to be falsy");
+            expect(err).toBeFalsy();
+            console.log("Test data to be truthy");
+            expect(seq).toBeTruthy();
+            console.log("Test data length to be correct");
+            expect(seq.length).toBe(50);
+            done();
+        });
+    });
+});
