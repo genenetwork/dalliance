409b7480f97bad782ae9b04903c23b95dc75ca38
Christian Fischer <christian@chfi.se>
2016-07-26 16:28:25 +0200

    Fixes to QTL source, add QTL & SNP density browser

--- js/qtl-source.js
+++ js/qtl-source.js
@@ -33,60 +33,29 @@ QtlMapSource.prototype.fetch = function(chr, min, max, scale, types, pool, callb
     var features = [];
     var prevPos = 0;
 
-    /*
-    var lrs = 0.1;
-    var lrsFeature = new DASFeature();
-    lrsFeature.min = 1;
-    lrsFeature.max = 125000000;
-    lrsFeature.score = lrs;
-    lrsFeature.type = "ruler";
-    features.push(lrsFeature);
-     */
+    this.lodCsv.fetch(null, function(results, error) {
+        if (results["Chr"] === chr) {
+            var feature = new DASFeature();
 
-    this.lodCsv.fetch({chr: chr}, function(results, error) {
-        var feature = new DASFeature();
+            feature.segment = chr;
+            var pos = results["Mb"];
 
-        feature.segment = chr;
-        var pos = results["pos"];
+            feature.min = pos * 1000000;
+            feature.max = pos * 1000000;
 
-        feature.min = pos * 1000000;
-        feature.max = pos * 1000000;
+            if (results["LRS"]) {
+                feature.score = results["LRS"];
+            } else if (results["LOD"]) {
+                feature.score = results["LOD"];
+            }
 
-        feature.score = results["lod"];
-
-        features.push(feature);
+            features.push(feature);
+        }
     }, function (results, error) {
         return callback(null, features, 1);
     });
 
 };
-
-/*
-QtlMapSource.prototype.getStylesheet = function(callback) {
-    var stylesheet = new DASStylesheet();
-
-    var rulerStyle = new DASStyle();
-    rulerStyle.glyph = "LINE";
-    rulerStyle.HEIGHT = 10;
-    rulerStyle.FGCOLOR = "red";
-    rulerStyle.BGCOLOR = "red";
-    var style = new DASStyle();
-    style.glyph = "HISTOGRAM";
-    style.HEIGHT = 500;
-    style.COLOR1 = "blue";
-    style.COLOR2 = style.COLOR1;
-    style.COLOR3 = style.COLOR1;
-    style.MIN = 0;
-    style.MAX = 5000;
-    style.STEPS = 100;
-    style.ZINDEX = 0;
-
-    // stylesheet.pushStyle({type: "ruler"}, null, rulerStyle);
-    stylesheet.pushStyle({type: "default"}, null, style);
-
-    return callback(stylesheet);
-};
-*/
 
 dalliance_registerSourceAdapterFactory(('qtl'), function(source) {
     return {
